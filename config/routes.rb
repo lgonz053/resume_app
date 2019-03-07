e962be5516985c74661f00eecb24250b631fbd04
anjelicakay <aktowner@gmail.com>
2019-03-07 14:30:14 -0600

    Completed routes, controller and views for students

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do 
     get '/students' => 'students#index'
+    post '/students' => 'students#create'
+    get '/students/:id' => 'students#show'
+    patch '/students/:id' => 'students#update'
   end
 end
