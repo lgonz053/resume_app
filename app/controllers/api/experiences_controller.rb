f5ece2db22977dc57905e658fda4ee42507bd845
pattywitwhy <pattyxhwang@gmail.com>
2019-03-07 11:43:30 -0600

    started models

--- app/controllers/api/experiences_controller.rb
+++ app/controllers/api/experiences_controller.rb
@@ -1,2 +1,8 @@
 class Api::ExperiencesController < ApplicationController
+  def index
+    @experiences = Experience.all
+
+    @experiences = @Experience.order(:id => :asc)
+    render 'index.json.jbuilder'
+  end
 end
