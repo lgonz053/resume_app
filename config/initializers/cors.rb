9690557ff0d6a41e52fdfe6e85b9db305fc4a402
Leo Gonzalez <lgonz053@gmail.com>
2019-03-08 17:27:27 -0600

    fixed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'example.com', 'ecstatic-golick-53422d.netlify.com', 'compassionate-wright-72e7d7.netlify.com/'
+    origins 'localhost:8080', 'example.com', 'https://ecstatic-golick-53422d.netlify.com', 'https://compassionate-wright-72e7d7.netlify.com'
 
     resource '*',
       headers: :any,
